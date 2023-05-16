850336adfa127c1d690797272af3066221b9c2f1
kieferperquin <91886036+kieferperquin@users.noreply.github.com>
2023-05-16 14:02:35 +0200

    Update ScoreCounter.cs

--- Assets/Scripts/Kiefer/Score/ScoreCounter.cs
+++ Assets/Scripts/Kiefer/Score/ScoreCounter.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class ScoreCounter : MonoBehaviour
 {
     public ScoreData score;
-    public bool RedShot = true;
+    public TurnManager turn;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "CupGoal")
         {
-            if (RedShot)
+            if (turn.curPlayer == 1)
             {
                 score.RedScore++;
+            }
+            else if (turn.curPlayer == 2)
+            {
+                score.BlueScore++;
             }
         }
     }
