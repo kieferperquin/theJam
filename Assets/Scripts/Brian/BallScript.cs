994bde372650784133a734cfa4a6382644a7cec7
C-h-a-o-z <b.somers@sintlucasedu.nl>
2023-05-16 11:41:35 +0200

    Ball improvements

--- Assets/Scripts/Brian/BallScript.cs
+++ Assets/Scripts/Brian/BallScript.cs
@@ -6,12 +6,15 @@ public class BallScript : MonoBehaviour
 {
     [SerializeField] int BounceLimit = 3;
     [SerializeField] int BouncedAmount = 0;
+    [SerializeField] float startDirX = 3;
+    [SerializeField] float startDirY = 3;
+    bool limitReached = false;
 
     // Start is called before the first frame update
     void Start()
     {
         //GetComponent<Rigidbody2D>().velocity = new Vector2(3, GetComponent<Rigidbody2D>().velocity.y);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(3, 3);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(startDirX, startDirY);
     }
 
     // Update is called once per frame
@@ -23,11 +26,22 @@ public class BallScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
+        if (limitReached)
+        {
+            Destroy(gameObject);
+        }    
+
         BouncedAmount += 1;
         Debug.Log("Bounced: " + BouncedAmount);
-        if (BouncedAmount > BounceLimit)
+        if (BouncedAmount >= BounceLimit)
         {
             Debug.Log("Over Limit");
+            limitReached = true;
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("HitSide");
+        Destroy(gameObject);
     }
 }
