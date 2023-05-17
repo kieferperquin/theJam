3495008e972312c8b4f184bfc22d37de17734af3
C-h-a-o-z <b.somers@sintlucasedu.nl>
2023-05-17 12:24:52 +0200

    some testing

---
+++ Assets/Scripts/Brian/Ray.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ray : MonoBehaviour
+{
+    public GameObject FollowLocation;
+    public bool enableRay = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enableRay == true)
+        {
+            gameObject.transform.position = FollowLocation.transform.position;
+            //Get the mouse position on the screen and send a raycast into the game world from that position.
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+            Debug.DrawLine(worldPoint, Vector2.zero);
+
+            //If something was hit, the RaycastHit2D.collider will not be null.
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
+}
