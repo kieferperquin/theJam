eca8eec445134dbd74d6616282fb9e9a9175f3f3
C-h-a-o-z <b.somers@sintlucasedu.nl>
2023-05-17 13:44:43 +0200

    Made aim rays FOR TESTING

--- Assets/Scripts/Brian/Ray.cs
+++ Assets/Scripts/Brian/Ray.cs
@@ -6,6 +6,7 @@ public class Ray : MonoBehaviour
 {
     public GameObject FollowLocation;
     public bool enableRay = false;
+    public Color rayColor = Color.white;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +19,27 @@ public class Ray : MonoBehaviour
         if (enableRay == true)
         {
             gameObject.transform.position = FollowLocation.transform.position;
+            gameObject.transform.rotation = FollowLocation.transform.rotation;
             //Get the mouse position on the screen and send a raycast into the game world from that position.
             //Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(3, 3), 1000);
-            Debug.DrawLine(transform.position, hit.point) ;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+            Debug.DrawLine(transform.position, hit.point, rayColor) ;
 
             //If something was hit, the RaycastHit2D.collider will not be null.
             if (hit.collider != null)
             {
                 Debug.Log(hit.collider.name);
+
+                Vector2 inDirection = Vector2.Reflect(transform.up, hit.normal);
+
+                Debug.DrawRay(hit.point, inDirection * 100, rayColor);
+                hit = Physics2D.Raycast(hit.point + hit.normal * 0.01f, inDirection);
+                if (hit != null && hit.collider != null)
+                {
+                    inDirection = Vector3.Reflect(inDirection, hit.normal);
+                    Debug.DrawRay(hit.point, inDirection * 100, rayColor);
+                }
+
             }
         }
     }
